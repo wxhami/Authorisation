6cfbd64be180c5e1d32bc9516b25e4744ee8728e
wxhami <d18102002@mail.ru>
2022-10-19 20:34:39 +0300

    add UserService

---
+++ Authorisation/UserService.cs
@@ -0,0 +1,25 @@
+﻿namespace Authorisation;
+
+public class UserService
+{
+    public List<User> Users;
+
+    public void CreateNewUser(string username, string password)
+    {
+        User newUser = new User(username, password);
+        Users.Add(newUser);
+    }
+
+    public User GetUser(string login)
+    {
+        foreach (var user in Users)
+        {
+            if (user.Username == login)
+            {
+                return user;
+            }
+        }
+
+        return null;
+    }
+}
