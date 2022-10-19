52db2416440ee4129a65abf185e00487cc6b91c8
wxhami <d18102002@mail.ru>
2022-10-19 20:35:34 +0300

    add constructor in User

--- Authorisation/User.cs
+++ Authorisation/User.cs
@@ -5,4 +5,11 @@ public class User
     public string Username;
     
     public string Password;
+
+    public User(string username, string password)
+    {
+        Username = username;
+        Password = password;
+    }
+    
 }
